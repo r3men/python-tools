545431c3eff7861f4d609ea5fea0008409367c35
Raymond Zhang <157990153+r3men@users.noreply.github.com>
2026-04-13 13:10:50 -0400

    Update iocextractor.py

--- scripts/iocextractor.py
+++ scripts/iocextractor.py
@@ -1,2 +1,24 @@
 # Script Name: IOC Extractor
 # Purpose: The purpose of this script is to reliably locate IP addresses, email addresses, and potentially malicious file hashes within an input file.
+
+import re
+import argparse
+
+def findIPS(text):
+  pattern = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+  ip_addresses = set(re.findall(pattern, text))
+  return ip_addresses
+  
+def findEmails(text):
+  emails = []
+  return emails
+  
+def findHashes():
+  hashes = [text]
+  return hashes
+
+def main():
+
+
+if "__name__" == "__main__":
+  main()
