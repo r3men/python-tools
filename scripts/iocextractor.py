52f505c316038968384abdde45412b8e8cd88b1b
Raymond Zhang <157990153+r3men@users.noreply.github.com>
2026-04-13 13:04:15 -0400

    Update iocextractor.py

--- scripts/iocextractor.py
+++ scripts/iocextractor.py
@@ -1,1 +1,2 @@
-
+# Script Name: IOC Extractor
+# Purpose: The purpose of this script is to reliably locate IP addresses, email addresses, and potentially malicious file hashes within an input file.
